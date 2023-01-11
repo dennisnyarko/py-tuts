c53b13c259343ef50bc0abecd37474a88797a98d
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-11 15:20:27 +0000

    loop n number of times

--- cat.py
+++ cat.py
@@ -8,4 +8,13 @@
 # for _ in range(10):
 #     print("meow")
 
-print("meow\n" * 3, end="")
+# print("meow\n" * 3, end="")
+
+# loop n number of times
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")
