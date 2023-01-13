1cdb6956b66c5a760c9b487ce47a4e3e6c76c156
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-13 13:17:38 +0000

    update (refactor)

--- mario.py
+++ mario.py
@@ -21,16 +21,7 @@ def main():
     print_square(3)
 
 def print_square(size):
-
-    #for each row in square
     for i in range(size):
-        
-        #for each brick in row
-        for j in range(size):
-
-            #print brick
-            print("#", end="")
-
-        print()
+        print("#" * size)
 
 main()
