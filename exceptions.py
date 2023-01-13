143adf5c4e8e9c76145610a13337778d06a4b561
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-13 14:37:13 +0000

    using loop for exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,17 @@
-try:    
-    x = int(input("What's x "))
-except ValueError:
-    print("x is not an integer")
-else:
-    print(f"x is {x}")
+# try:    
+#     x = int(input("What's x "))
+# except ValueError:
+#     print("x is not an integer")
+# else:
+#     print(f"x is {x}")
+
+#using while loop for exceptions
+while True:
+    try:    
+        x = int(input("What's x "))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+
+print(f"x is {x}")    
