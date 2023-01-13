770edf05551fdac621a95a437c908bae5a8b02e6
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-13 12:57:54 +0000

    update (horizontal)

--- mario.py
+++ mario.py
@@ -1,8 +1,18 @@
-def main():
-    print_column(3)
+# #vertical loop
+# def main():
+#     print_column(3)
 
-#loop different way
-def print_column(height):
-    print("#\n" * height, end="")
+# #loop different way (vertical)
+# def print_column(height):
+#     print("#\n" * height, end="")
     
+# main()
+
+#horizontal loop
+def main():
+    print_row(4)
+
+def print_row(width):
+    print("?" * width)
+
 main()
