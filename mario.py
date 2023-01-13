5487be7d06e45bae5533f02925b733f0e79a4d8f
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-13 13:15:47 +0000

    loop (rows * columns)

--- mario.py
+++ mario.py
@@ -9,10 +9,28 @@
 # main()
 
 #horizontal loop
-def main():
-    print_row(4)
+# def main():
+#     print_row(4)
 
-def print_row(width):
-    print("?" * width)
+# def print_row(width):
+#     print("?" * width)
+
+# main()
+
+def main():
+    print_square(3)
+
+def print_square(size):
+
+    #for each row in square
+    for i in range(size):
+        
+        #for each brick in row
+        for j in range(size):
+
+            #print brick
+            print("#", end="")
+
+        print()
 
 main()
