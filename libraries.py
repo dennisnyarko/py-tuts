8162814da56c56c2c7311f9439dbc7488d63bbb5
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-14 23:53:55 +0000

    shuffle

--- libraries.py
+++ libraries.py
@@ -5,5 +5,13 @@ import random
 # print(coin)
 
 #generate random number
-number = random.randint(1, 10)
-print(number)
+# number = random.randint(1, 10)
+# print(number)
+
+
+#shuffle
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+
+for card in cards:
+    print(card)
