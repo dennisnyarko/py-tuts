786cdd27b95930fdc58b1e22e2436d75e14a5c7e
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-16 17:49:14 +0000

    use packages with APIs

--- packages.py
+++ packages.py
@@ -1,5 +1,13 @@
-import cowsay
+import json
+import requests
 import sys
 
-if len(sys.argv) == 2:
-    cowsay.trex("hello, " + sys.argv[1])
+if len(sys.argv) != 2:
+    sys.exit()
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+#print(json.dumps(response.json(), indent=2))
+
+k = response.json()
+for result in k["results"]:
+    print(result["trackName"])
