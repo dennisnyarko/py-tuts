8fc290a92e886e3c86cad2c7edcb7629095e4eb4
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-16 16:45:29 +0000

    update

--- cmd-line.py
+++ cmd-line.py
@@ -1,6 +1,7 @@
 import sys
 
-try:
-    print("hello, my name is", sys.argv[1])
-except IndexError:
+if len(sys.argv) < 2:
     print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+print("hello, my name is", sys.argv[1])
