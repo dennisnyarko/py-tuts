9dc99c6033f0ce52f72496387ab6c45e4e49c9a1
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-16 16:46:59 +0000

    update

--- cmd-line.py
+++ cmd-line.py
@@ -4,4 +4,5 @@ if len(sys.argv) < 2:
     print("Too few arguments")
 elif len(sys.argv) > 2:
     print("Too many arguments")
-print("hello, my name is", sys.argv[1])
+else:
+    print("hello, my name is", sys.argv[1])
