8e38590af52d5ab762e38db7850e01f4af8ab49c
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-18 23:21:38 +0000

    write to text file

--- file_io.py
+++ file_io.py
@@ -1,6 +1,12 @@
-names = []
-for _ in range(3):
-    names.append(input("what's your name? "))
+# names = []
+# for _ in range(3):
+#     names.append(input("what's your name? "))
 
-for name in sorted(names):
-    print(f"hello, {name}")
+# for name in sorted(names):
+#     print(f"hello, {name}")
+
+name = input("What's your name? ")
+
+file = open("names.txt", "w")
+file.write(name)
+file.close()
