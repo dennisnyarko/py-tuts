7a8abfb58c1ee5f005a51c5c0b93ada895c578d4
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-28 23:58:10 +0000

    add comment

--- twitter.py
+++ twitter.py
@@ -3,10 +3,6 @@ import re
 #accept input & strip unnecessary part
 url = input("URL: ").strip()
 
-<<<<<<< HEAD
-#check input to match set format
-=======
 #conditional statement to
->>>>>>> 9432319 (add comments)
 if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
     print(f"Username:" , matches.group(1))
