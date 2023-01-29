0fd73287db97e4bcfa434588e6d26d5b19ed23a6
Dennis Nyarko <96777791+dennisnyarko@users.noreply.github.com>
2023-01-29 00:00:29 +0000

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -3,5 +3,6 @@ import re
 #accept input & strip unnecessary part
 url = input("URL: ").strip()
 
+#check input to match set format
 if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
     print(f"Username:" , matches.group(1))
