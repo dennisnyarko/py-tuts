88e698348a2a3c26a4ee290b722fedff6a16c3dc
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-02 12:10:52 +0000

    update

--- oop.py
+++ oop.py
@@ -1,11 +1,12 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
@@ -19,7 +20,8 @@ def main():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
 
 
 
