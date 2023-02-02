97b04e08cf25065c45a2b263e22f13fc418e0d86
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-02 14:21:51 +0000

    correct errors

--- oop.py
+++ oop.py
@@ -2,21 +2,22 @@ class Student:
     def __init__(self, name, house):
         if not name:
             raise ValueError("Missing name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
+        
         self.name = name
         self.house = house
 
     def __str__(self):
         return f"{self.name} from {self.house}"
 
-    #Getter
+    @property
     def house(self):
-        return self.house
+        return self._house
     
-    #Setter
+    @house.setter
     def house(self, house):
-        self.house = house
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
 
 
 def main():
