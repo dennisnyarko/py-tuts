9b2284fbc9fcdc6f0bd79be039006032eef7a136
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-04 23:30:02 +0000

    update

--- oop.py
+++ oop.py
@@ -7,25 +7,7 @@ class Student:
     def __str__(self):
         return f"{self.name} from {self.house}"
 
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, name):
-        if not name:
-            raise ValueError("Missing name")
-        self._name = name
     
-    @property
-    def house(self):
-        return self._house
-    
-    @house.setter
-    def house(self, house):
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
-        self._house = house
 
 
 def main():
