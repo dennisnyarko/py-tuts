36f47c0ee886f6e1f90a3cfdb6ab32a229131091
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-05 22:35:41 +0000

    update

--- wizard.py
+++ wizard.py
@@ -7,11 +7,9 @@ class Wizard:
 
 class Student:
     def __init__(self, name, house):
-        self.name = name
         self.house = house
 
 
 class Professor:
     def __init__(self, name, subject):
-        self.name = name
         self.subject = subject
