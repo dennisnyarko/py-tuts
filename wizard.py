0d1dc178426ca21efa20843bdfb2b0917b5c7c50
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-05 22:38:51 +0000

    update

--- wizard.py
+++ wizard.py
@@ -7,9 +7,11 @@ class Wizard:
 
 class Student(Wizard):
     def __init__(self, name, house):
+        super().__init__(name)
         self.house = house
 
 
 class Professor(Wizard):
     def __init__(self, name, subject):
+        super().__init__(name)
         self.subject = subject
