3f579cb88ee4972b6b10b95db2f0a9ba97525f12
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-09 13:26:04 +0000

    use enumerate

--- gryffindors.py
+++ gryffindors.py
@@ -1,15 +1,20 @@
-students = [
-    {"name": "Hermione", "house": "Gryffindor"},
-    {"name": "Harry", "house": "Gryffindor"},
-    {"name": "Ron", "house": "Gryffindor"},
-    {"name": "Draco", "house": "Slytherin"},
-    {"name": "Padma", "house": "Ravenclaw"},
-]
+# students = [
+#     {"name": "Hermione", "house": "Gryffindor"},
+#     {"name": "Harry", "house": "Gryffindor"},
+#     {"name": "Ron", "house": "Gryffindor"},
+#     {"name": "Draco", "house": "Slytherin"},
+#     {"name": "Padma", "house": "Ravenclaw"},
+# ]
 
-def is_gryffindor(s):
-    return s["house"] == "Gryffindor"
+# def is_gryffindor(s):
+#     return s["house"] == "Gryffindor"
 
-gryffindors = filter(is_gryffindor, students)
+# gryffindors = filter(is_gryffindor, students)
 
-for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
-    print(gryffindor["name"])
+# for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+#     print(gryffindor["name"])
+
+students = ["Hermione", "Harry", "Ron"]
+
+for i, student in enumerate(students):
+    print(i + 1, student)
