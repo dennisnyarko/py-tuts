f09c7ad7630f8a808f09566c646b438c6f91edb8
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-02-09 13:30:20 +0000

    indent update

--- sleep.py
+++ sleep.py
@@ -1,3 +1,7 @@
-n = int(input("What's n? "))
-for i in range(n):
-    print("🐑" * i)
+def main():
+    n = int(input("What's n? "))
+    for i in range(n):
+        print("🐑" * i)
+
+if __name__ == "__main__":
+    main()
